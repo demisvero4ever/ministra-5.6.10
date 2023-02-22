44fa07fcf754c4dc475e612376d9d471caa0fa3a
JerryLutor <ave_servis@ukr.net>
2023-02-22 14:40:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ echo ""
 while true; do
     read -p "Do you want to install Ministra direct from their servers? " yn
     case $yn in
-        [Yy]* ) ministra_source=http://download.ministra.com/downloads/159934057961c4dfe9153ee02d7e3fb1/ministra-5.6.9.zip; break;;
-        [Nn]* ) ministra_source=http://185.25.27.50:88/Ministra_TV_Platform_5.6.8.zip; break;;
+        [Yy]* ) ministra_source=http://download.ministra.com/downloads/159934057961c4dfe9153ee02d7e3fb1/ministra-5.6.10.zip; break;;
+        [Nn]* ) ministra_source=http://xxxx/Ministra_TV_Platform_5.6.8.zip; break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
